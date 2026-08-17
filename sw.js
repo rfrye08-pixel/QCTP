20b13d15eaf942ce16f152c1f222a421d1e6e976
rfrye08-pixel <rfrye08@gmail.com>
2026-08-17 12:19:29 -0500

    Refresh QCTP offline shell for Rev1.1.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'qctp-shell-v4';
-const SHELL = ['./', './index.html', './styles.css?v=1.1.1', './app.js?v=1.1.1', './hotfix-1.1.1.js?v=1.1.1', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'qctp-shell-v5';
+const SHELL = ['./', './index.html', './styles.css?v=1.1.2', './app.js?v=1.1.2', './hotfix-1.1.1.js?v=1.1.2', './hotfix-1.1.2.js?v=1.1.2', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
